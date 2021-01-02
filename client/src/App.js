0de59658aba22d03d339a9c4109a7cea3c1742a0
sugarrobert <robert.sugar@hotmail.com>
2021-01-02 16:47:38 +0100

    added loading user & set token

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,12 @@ import Alerts from './components/layout/Alerts';
 import ContactState from './context/contact/ContactState';
 import AuthtState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
